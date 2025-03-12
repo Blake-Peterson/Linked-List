38a9bce278182d0e21db48c231e17b2bdcee174a
Blake Peterson <121067208+Blake-Peterson@users.noreply.github.com>
2025-03-12 07:25:40 -0500

    initial commit for morning

---
+++ src/linked_list.js
@@ -0,0 +1,72 @@
+class LinkedList{
+
+    head = null;
+    tail = null;
+    size = 0;
+
+    append(value){
+
+    }
+
+    prepend(value){
+
+    }
+
+    at(index){
+
+    }
+
+    pop(){
+
+    }
+
+    contains(value){
+
+    }
+
+    find(value){
+
+    }
+
+    toString(){
+        
+    }
+}
+
+class Node {
+    constructor(value, nextNode){
+        this.value = null;
+        this.nextNode = null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const list = new LinkedList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
