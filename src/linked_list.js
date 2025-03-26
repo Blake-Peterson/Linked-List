d470544dce1bebf341fa1840c85f54935e487d28
Blake <121067208+Blake-Peterson@users.noreply.github.com>
2025-03-25 21:28:29 -0500

    added functionality

--- src/linked_list.js
+++ src/linked_list.js
@@ -5,31 +5,99 @@ class LinkedList{
     size = 0;
 
     append(value){
-
+        if(size==0){
+            head = new Node(value, null);
+        }
+        
+        if(this.tail===null && this.head==null){
+          let new_node = new Node(value,null);
+          this.head.value = value;
+        } 
+        size++;
     }
 
     prepend(value){
+        if(size==0){
+            head = new Node(value, null);
+        } else {
+            let new_head = new Node(value,null);
+            current = this.head;
+            new_head.nextNode = current;
+            this.head = new_head;
+        }
+        this.size++;
+    }
 
+    get size(){
+        return this.size;
+    }
+
+    get head(){
+        return this.head;
+    }
+
+    get tail(){
+        return this.tail;
     }
 
     at(index){
-
+        if(size==0){
+            return null;
+        }
+        let current = this.head;
+        for(let i=0;i<index;i++){
+            current = current.nextNode;
+        }
+        return current.value;
     }
 
     pop(){
-
+        let current = this.head;
+        let index = 0;
+        while(current.nextNode != null){
+            index++;
+            current = current.nextNode;
+            if(index+1== this.size){
+                current.nextNode = null;
+            }
+        }
     }
 
     contains(value){
-
+        let current = this.head.nextNode;
+        while(current.nextNode != null){
+            if (current.value == value){
+                return true;
+            }
+        }
+        return false;
     }
 
     find(value){
-
+        let current = this.head.nextNode;
+        let index = 0;
+        while(current.nextNode != null){
+            if (current.value == value){
+                return index;
+            } else {
+                index++;
+            }
+        }
+        return null;
     }
 
     toString(){
-        
+      let current = this.head;  
+      let linked_string = "";
+      while(current.nextNode != null){
+        if(current.value !=null){
+            linked_string +=  "( "+ value +" ) -> ";
+            current = current.nextNode;
+        } else{
+            linked_string += "null";
+        }
+      }
+      return linked_string;
     }
 }
 
@@ -39,24 +107,6 @@ class Node {
         this.nextNode = null;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
